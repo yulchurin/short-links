f57907df0fdd156f99a5bbe9e653f74024424c44
Tayfur Yulchurin <yulchurin@gmail.com>
2025-03-31 02:42:33 +0500

    fix paths

--- src/ShortLinksServiceProvider.php
+++ src/ShortLinksServiceProvider.php
@@ -9,13 +9,13 @@ class ShortLinksServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'./config/short-links.php' => config_path('short-links.php'),
+            __DIR__.'/config/short-links.php' => config_path('short-links.php'),
         ]);
 
-        $this->loadRoutesFrom(__DIR__.'./routes/shortener.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/shortener.php');
 
         $this->publishesMigrations([
-            __DIR__.'./database/migrations' => database_path('migrations'),
+            __DIR__.'/database/migrations' => database_path('migrations'),
         ]);
     }
 }
